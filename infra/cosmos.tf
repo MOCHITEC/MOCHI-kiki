583f792707e7ca351ba78a9c0403cabf9772e9a2
MettaDora <hendra.guntur88@gmail.com>
2026-05-13 07:42:38 +0900

    chore(infra): add Cosmos DB account with meeting_db database and 5 containers

---
+++ infra/cosmos.tf
@@ -0,0 +1,123 @@
+# infra/cosmos.tf
+
+resource "azurerm_cosmosdb_account" "main" {
+  name                = "${var.prefix}-cosmos-${var.environment}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  consistency_policy {
+    consistency_level = var.cosmos_consistency_level
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.main.location
+    failover_priority = 0
+  }
+
+  tags = azurerm_resource_group.main.tags
+}
+
+resource "azurerm_cosmosdb_sql_database" "meeting_db" {
+  name                = "meeting_db"
+  resource_group_name = azurerm_resource_group.main.name
+  account_name        = azurerm_cosmosdb_account.main.name
+}
+
+resource "azurerm_cosmosdb_sql_container" "utterances" {
+  name                = "utterances"
+  resource_group_name = azurerm_resource_group.main.name
+  account_name        = azurerm_cosmosdb_account.main.name
+  database_name       = azurerm_cosmosdb_sql_database.meeting_db.name
+  partition_key_path  = "/meeting_id"
+
+  indexing_policy {
+    indexing_mode = "consistent"
+
+    included_path {
+      path = "/*"
+    }
+  }
+}
+
+resource "azurerm_cosmosdb_sql_container" "meetings" {
+  name                = "meetings"
+  resource_group_name = azurerm_resource_group.main.name
+  account_name        = azurerm_cosmosdb_account.main.name
+  database_name       = azurerm_cosmosdb_sql_database.meeting_db.name
+  partition_key_path  = "/id"
+
+  indexing_policy {
+    indexing_mode = "consistent"
+
+    included_path {
+      path = "/*"
+    }
+  }
+}
+
+resource "azurerm_cosmosdb_sql_container" "clarification_sessions" {
+  name                = "clarification_sessions"
+  resource_group_name = azurerm_resource_group.main.name
+  account_name        = azurerm_cosmosdb_account.main.name
+  database_name       = azurerm_cosmosdb_sql_database.meeting_db.name
+  partition_key_path  = "/meeting_id"
+
+  indexing_policy {
+    indexing_mode = "consistent"
+
+    included_path {
+      path = "/*"
+    }
+  }
+}
+
+resource "azurerm_cosmosdb_sql_container" "consents" {
+  name                = "consents"
+  resource_group_name = azurerm_resource_group.main.name
+  account_name        = azurerm_cosmosdb_account.main.name
+  database_name       = azurerm_cosmosdb_sql_database.meeting_db.name
+  partition_key_path  = "/meeting_id"
+
+  indexing_policy {
+    indexing_mode = "consistent"
+
+    included_path {
+      path = "/*"
+    }
+  }
+}
+
+resource "azurerm_cosmosdb_sql_container" "agent_logs" {
+  name                = "agent_logs"
+  resource_group_name = azurerm_resource_group.main.name
+  account_name        = azurerm_cosmosdb_account.main.name
+  database_name       = azurerm_cosmosdb_sql_database.meeting_db.name
+  partition_key_path  = "/meeting_id"
+
+  indexing_policy {
+    indexing_mode = "consistent"
+
+    included_path {
+      path = "/*"
+    }
+  }
+}
+
+# Key Vault にエンドポイントとキーを保存する
+resource "azurerm_key_vault_secret" "cosmos_endpoint" {
+  name         = "azure-cosmos-endpoint"
+  value        = azurerm_cosmosdb_account.main.endpoint
+  key_vault_id = azurerm_key_vault.main.id
+
+  depends_on = [azurerm_key_vault_access_policy.terraform_operator]
+}
+
+resource "azurerm_key_vault_secret" "cosmos_key" {
+  name         = "azure-cosmos-key"
+  value        = azurerm_cosmosdb_account.main.primary_key
+  key_vault_id = azurerm_key_vault.main.id
+
+  depends_on = [azurerm_key_vault_access_policy.terraform_operator]
+}
